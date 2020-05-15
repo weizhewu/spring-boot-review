244e850fd9b276bbcb73c95c672d3c6bead19a75
wzw <weizhewu232623@gmail.com>
2020-05-15 19:04:49 +0800

    给生日用户发送祝福卡片

--- spring-boot-task-schedule/src/main/java/com/soft1851/springboot/task/schedule/task/Task2.java
+++ spring-boot-task-schedule/src/main/java/com/soft1851/springboot/task/schedule/task/Task2.java
@@ -1,10 +1,16 @@
 package com.soft1851.springboot.task.schedule.task;
 
+import com.soft1851.springboot.task.schedule.dao.MailService;
+import com.soft1851.springboot.task.schedule.model.User;
+import com.soft1851.springboot.task.schedule.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import javax.annotation.Resource;
+import javax.mail.MessagingException;
+
+import java.util.List;
 
 /**
  * @Author: zw_w
@@ -13,10 +19,21 @@ import java.time.format.DateTimeFormatter;
  */
 @Component
 public class Task2 {
-    private DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy年MM月dd日 HH:mm:ss");
+    @Resource
+    private UserRepository userRepository;
+    @Resource
+    private MailService mailService;
 
-    @Scheduled(fixedRate = 3000)
-    public void reportCurrentTime() {
-        System.out.println("现在时间：" + dtf.format( LocalDateTime.now()));
+    @Scheduled(cron = "0 57 18 * * ?")
+    public void reportCurrentTime() throws MessagingException {
+        List<User> users = userRepository.findUsersByBirthday();
+        String to = "weizhewu232623@gmail.com";
+        String subject = "生日快乐";
+        String content = "<div id=\"app\"><div style=\"width: 400px;height: 450px;padding: 10px 10px;border: 1px solid #eee;box-shadow: -2px -2px 10px rgba(0, 0, 0, 0.5);margin: auto;\"><div style=\"width: 100%;height: 200px;\"><img src=\"https://i.loli.net/2020/05/12/dVMKqUEn5wfusX4.jpg\" alt=\"\" style=\"width: 100%;height: 100%;\" /></div><div style=\"width: 100%;height: 200px;margin-top: 30px;\"><span>Hi</span><p class=\"con-body\" style=\"text-indent: 2em;\">有些事情可能你已经忘记，但我们依然记得。</br>今天是你的生日，我们唱了一首歌送给你，希望它能给你带来一些惊喜和欢乐。Happy Birthday。</p><div style=\"text-align: center;margin-top: 30px;\"><button style=\"width: 160px;height: 45px;background: rgb(80, 151, 239); border-radius: 4px;color: white;border: none;\">开始</button></div></div> </div></div>";
+        for (User user : users){
+            mailService.sendHtmlMail(to,user.getNickName()+subject,content);
+            System.out.println("邮件已发送");
+        }
+//        System.out.println("现在时间：" + dtf.format( LocalDateTime.now()));
     }
 }
